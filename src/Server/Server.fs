bf4e57bbc9ba4e827d099df0f44bfdb349d84686
Taimour <mukhay@ttu.ee>
2024-10-02 13:59:31 +0300

    initialize project

---
+++ src/Server/Server.fs
@@ -0,0 +1,45 @@
+module Server
+
+open SAFE
+open Saturn
+open Shared
+
+module Storage =
+    let todos =
+        ResizeArray [
+            Todo.create "Create new SAFE project"
+            Todo.create "Write your app"
+            Todo.create "Ship it!!!"
+        ]
+
+    let addTodo todo =
+        if Todo.isValid todo.Description then
+            todos.Add todo
+            Ok()
+        else
+            Error "Invalid todo"
+
+let todosApi ctx = {
+    getTodos = fun () -> async { return Storage.todos |> List.ofSeq }
+    addTodo =
+        fun todo -> async {
+            return
+                match Storage.addTodo todo with
+                | Ok() -> todo
+                | Error e -> failwith e
+        }
+}
+
+let webApp = Api.make todosApi
+
+let app = application {
+    use_router webApp
+    memory_cache
+    use_static "public"
+    use_gzip
+}
+
+[<EntryPoint>]
+let main _ =
+    run app
+    0
