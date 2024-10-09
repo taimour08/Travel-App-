1aeb568c55e52e91a6ca55db875300ede4eee083
Taimour <mukhay@ttu.ee>
2024-10-09 22:09:58 +0300

    understand app.fs and elmish

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -1,3 +1,6 @@
+(*App.fs: Contains the main Elmish application logic for the 
+front-end, including the model, update, and view functions.*)
+
 module App
 
 open Elmish
@@ -18,3 +21,13 @@ Program.mkProgram Index.init Index.update Index.view
 |> Program.withReactSynchronous "elmish-app"
 
 |> Program.run
+
+
+(*
+Elmish is a simple way to manage the state of web applications using F#. It follows the Model-View-Update (MVU) pattern, which helps organize your code by separating how the app looks (the view), how the app's state changes (the update), and what the current state is (the model).
+
+Key Concepts:
+Model: This is the current state of your app. For example, whether a button has been clicked or not.
+View: This is how your app looks, based on the current state. For example, if the button was clicked, it might show some new text.
+Update: This is the logic that updates your model when something happens, like a button click.
+*)
